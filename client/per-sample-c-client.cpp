3dc8f1db0171edfd85fe2171f6294330b20a0a0a
Karl Yerkes <karl.yerkes@gmail.com>
2019-10-26 19:35:16 -0700

    added compile/runtime checks in client

--- client/per-sample-c-client.cpp
+++ client/per-sample-c-client.cpp
@@ -1,24 +1,107 @@
 #include <lo/lo.h>
 
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include "libtcc.h"
+using ProcessFunc = void (*)(double time, float *input, float *output);
+using std::string;
 std::string slurp(const std::string &fileName);
 std::string slurp();
 
+unsigned SAMPLE_RATE = 44100;
+
+string header = R"(
+double sin(double);
+double cos(double);
+double exp(double);
+double log(double);
+double tanh(double);
+double log2(double);
+double log10(double);
+double modf(double x, double* intpart);
+double pow(double, double);
+double sqrt(double);
+double fmod(double numer, double denom);
+double fabs(double x);
+)";
+
 int main(int argc, char *argv[]) {
+  // use command line arguments to...
+  // set destination address and port (defaults 127.0.0.1:9010)
+  // disable compile checking or other processes
+
   std::string sourceCode;
   if (argc > 1)
     sourceCode = slurp(argv[1]);
   else
     sourceCode = slurp();
 
+  // try to compile (time this!)
+  // if it fails, report error and exit
+  // run a few times in case it crashes (time this?!)
+  //
+
+  TCCState *instance = tcc_new();
+  tcc_set_options(instance, "-Wall -Werror -nostdinc -nostdlib");
+  tcc_set_output_type(instance, TCC_OUTPUT_MEMORY);
+
+  char buffer[10];
+  sprintf(buffer, "%u", SAMPLE_RATE);
+  tcc_define_symbol(instance, "SAMPLE_RATE", buffer);
+  sprintf(buffer, "%lf", M_PI);
+  tcc_define_symbol(instance, "M_PI", buffer);
+
+  string actual = header + sourceCode;
+  if (0 != tcc_compile_string(instance, actual.c_str())) {
+    exit(1);
+  }
+  tcc_add_library(instance, "m");
+  int size = tcc_relocate(instance, nullptr);
+
+  if (-1 == tcc_relocate(instance, TCC_RELOCATE_AUTO)) {
+    printf("Relocate failed!");
+    exit(1);
+  }
+
+  ProcessFunc function = (ProcessFunc)tcc_get_symbol(instance, "process");
+  if (function == nullptr) {
+    printf("No process callback found.");
+    exit(1);
+  }
+
+  using InitFunc = void (*)(void);
+  InitFunc init = (InitFunc)tcc_get_symbol(instance, "begin");
+  if (init) init();
+
+  ////////
+  // better that this crash now than it crash the server
+  float i[8] = {0};
+  float o[8] = {0};
+  for (int n = 0; n < 44100; n++)  //
+    function((double)n / SAMPLE_RATE, i, o);
+
+  // run pre-processor to remove comments and include files
+  //
+  // libtcc lets us run the preprocessor when we pass TCC_OUTPUT_PREPROCESS to
+  // tcc_set_output_type(), but this spits the code out the standard output. we
+  // must intercept this output to get it into a string. also, we might want to
+  // do this step before anything else?
+  //
+  // https://stackoverflow.com/questions/5419356/redirect-stdout-stderr-to-a-string
+  //
+
+  // maybe...
+  //   minify (instead of clang-format)
+  //   extract "modeline" commands
+
   lo_blob b = lo_blob_new(sourceCode.size(), sourceCode.data());
   lo_address t = lo_address_new(nullptr, "9010");
   if (!lo_send(t, "/code", "b", b)) {
-    printf("failed to send packet\n");
+    printf("failed to send packet.");
     fflush(stdout);
     exit(1);
   }
@@ -52,4 +135,3 @@ std::string slurp() {
     cin_str.insert(cin_str.end(), buffer.data(), buffer.data() + cnt_char);
   return string(cin_str.data(), cin_str.size());
 }
-
