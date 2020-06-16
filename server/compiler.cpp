c0ad67a01581eaf610a3a4ab217d6bc9f275c90e
Karl Yerkes <karl.yerkes@gmail.com>
2020-06-16 10:10:43 -0700

    separate static/dynamic linked tcc

--- server/compiler.cpp
+++ server/compiler.cpp
@@ -1,12 +1,12 @@
 #include "compiler.h"
 
+#include <dlfcn.h>
+#include <libtcc.h>
 #include <math.h>
 
 #include <mutex>
 #include <regex>
 #include <string>
-
-#include "libtcc.h"
 
 #include "../HostInterface.h"
 
@@ -18,50 +18,76 @@ extern unsigned SAMPLE_RATE;
 // Tiny C Compiler ///////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-void tcc_error_handler(void* str, const char* msg) {
-  static_cast<string*>(str)->append(msg);
+void error_handler(void *str, const char *msg) {
+  static_cast<string *>(str)->append(msg);
 }
 
+typedef void (*TCCErrorFunc)(void *opaque, const char *msg);
+
+typedef TCCState *(*type_tcc_new)(void);
+typedef void (*type_tcc_delete)(TCCState *);
+typedef void (*type_tcc_set_error_func)(TCCState *, void *, TCCErrorFunc);
+typedef void (*type_tcc_set_options)(TCCState *, const char *);
+typedef int (*type_tcc_set_output_type)(TCCState *, int);
+typedef void (*type_tcc_define_symbol)(TCCState *, const char *, const char *);
+typedef int (*type_tcc_compile_string)(TCCState *, const char *);
+typedef int (*type_tcc_add_symbol)(TCCState *, const char *, const void *);
+typedef int (*type_tcc_add_library)(TCCState *, const char *);
+typedef int (*type_tcc_relocate)(TCCState *, void *);
+typedef void *(*type_tcc_get_symbol)(TCCState *, const char *);
+
 struct TCC {
-  TCCState* instance{nullptr};
+  type_tcc_new _new;
+  type_tcc_delete _delete;
+  type_tcc_set_error_func _set_error_func;
+  type_tcc_set_options _set_options;
+  type_tcc_set_output_type _set_output_type;
+  type_tcc_define_symbol _define_symbol;
+  type_tcc_compile_string _compile_string;
+  type_tcc_add_symbol _add_symbol;
+  type_tcc_add_library _add_library;
+  type_tcc_relocate _relocate;
+  type_tcc_get_symbol _get_symbol;
+
+  TCCState *instance{nullptr};
   ProcessFunc function{nullptr};
   size_t size{0};
 
   ~TCC() { maybe_destroy(); }
   void maybe_destroy() {
     if (instance) {
-      tcc_delete(instance);
+      _delete(instance);
 
       // tcc_delete does not null the pointer
       instance = nullptr;
     }
   }
 
-  bool compile(const string& code, string* err) {
+  bool compile(const string &code, string *err) {
     maybe_destroy();
-    instance = tcc_new();
+    instance = _new();
     if (instance == nullptr) {
       err->append("Null Instance");
       return false;
     }
 
-    tcc_set_error_func(instance, (void*)err, tcc_error_handler);
+    _set_error_func(instance, (void *)err, error_handler);
 
     // treat warnings as errors
-    tcc_set_options(instance, "-Wall -Werror -nostdinc -nostdlib");
-    // tcc_set_options(instance, "-Wall -Werror");
+    //_set_options(instance, "-Wall -Werror -nostdinc -nostdlib");
+    _set_options(instance, "-Wall -Werror");
 
-    tcc_set_output_type(instance, TCC_OUTPUT_MEMORY);
+    _set_output_type(instance, TCC_OUTPUT_MEMORY);
 
     // Define Pre-Processor Symbols
     //
     char buffer[10];
     sprintf(buffer, "%u", SAMPLE_RATE);
-    tcc_define_symbol(instance, "SAMPLE_RATE", buffer);
+    _define_symbol(instance, "SAMPLE_RATE", buffer);
 
     // Do the compile step
     //
-    if (0 != tcc_compile_string(instance, code.c_str())) {
+    if (0 != _compile_string(instance, code.c_str())) {
       // TODO: move all this to the client. the server should never fail to
       // compile now because the client does this check
 
@@ -90,31 +116,33 @@ struct TCC {
       return false;
     }
     // add symbols
-    tcc_add_symbol(instance, "host_reset", (void*)host_reset);
-    tcc_add_symbol(instance, "host_float", (void*)host_float);
-    tcc_add_symbol(instance, "host_int", (void*)host_int);
-    tcc_add_symbol(instance, "host_char", (void*)host_char);
+    _add_symbol(instance, "host_reset", (void *)host_reset);
+    _add_symbol(instance, "host_float", (void *)host_float);
+    _add_symbol(instance, "host_int", (void *)host_int);
+    _add_symbol(instance, "host_char", (void *)host_char);
 
     // tcc_add_symbol(instance, "log", (void*)logf);
     // tcc_add_symbol(instance, "log", (void*)logf);
-    tcc_add_library(instance, "m");
+    //_add_library(instance, "System");
+    int result = _add_library(instance, "m");
+    printf("got %d\n", result);
 
     // XXX broken on linux
     // size = tcc_relocate(instance, nullptr);
 
-    if (-1 == tcc_relocate(instance, TCC_RELOCATE_AUTO)) {
+    if (-1 == _relocate(instance, TCC_RELOCATE_AUTO)) {
       err->append("Relocate Failed");
       return false;
     }
 
-    function = (ProcessFunc)tcc_get_symbol(instance, "process");
+    function = (ProcessFunc)_get_symbol(instance, "process");
     if (function == nullptr) {
       err->append("No 'process' Symbol");
       return false;
     }
 
     using InitFunc = void (*)(void);
-    InitFunc init = (InitFunc)tcc_get_symbol(instance, "begin");
+    InitFunc init = (InitFunc)_get_symbol(instance, "begin");
     if (init) init();
 
     return true;
@@ -132,7 +160,62 @@ struct SwappingCompilerImplementation {
   bool shouldSwap{false};
   int size;
 
-  bool compile(const string& code, string* err) {
+  SwappingCompilerImplementation() {
+#ifdef USE_DYNAMIC
+    void *handle = dlopen("libtcc.dylib", RTLD_NOW);
+
+    if (handle == nullptr) {
+      printf("DLOPEN ERROR:%s\n", dlerror());
+      exit(1);
+    }
+
+    tcc[0]._new = (type_tcc_new)dlsym(handle, "tcc_new");
+    tcc[0]._delete = (type_tcc_delete)dlsym(handle, "tcc_delete");
+    tcc[0]._set_error_func =
+        (type_tcc_set_error_func)dlsym(handle, "tcc_set_error_func");
+    tcc[0]._set_options =
+        (type_tcc_set_options)dlsym(handle, "tcc_set_options");
+    tcc[0]._set_output_type =
+        (type_tcc_set_output_type)dlsym(handle, "tcc_set_output_type");
+    tcc[0]._define_symbol =
+        (type_tcc_define_symbol)dlsym(handle, "tcc_define_symbol");
+    tcc[0]._compile_string =
+        (type_tcc_compile_string)dlsym(handle, "tcc_compile_string");
+    tcc[0]._add_symbol = (type_tcc_add_symbol)dlsym(handle, "tcc_add_symbol");
+    tcc[0]._add_library =
+        (type_tcc_add_library)dlsym(handle, "tcc_add_library");
+    tcc[0]._relocate = (type_tcc_relocate)dlsym(handle, "tcc_relocate");
+    tcc[0]._get_symbol = (type_tcc_get_symbol)dlsym(handle, "tcc_get_symbol");
+#else
+    tcc[0]._new = tcc_new;
+    tcc[0]._delete = tcc_delete;
+    tcc[0]._set_error_func = tcc_set_error_func;
+    tcc[0]._set_options = tcc_set_options;
+    tcc[0]._set_output_type = tcc_set_output_type;
+    tcc[0]._define_symbol = tcc_define_symbol;
+    tcc[0]._compile_string = tcc_compile_string;
+    tcc[0]._add_symbol = tcc_add_symbol;
+    tcc[0]._add_library = tcc_add_library;
+    tcc[0]._relocate = tcc_relocate;
+    tcc[0]._get_symbol = tcc_get_symbol;
+#endif
+
+    tcc[1]._new = tcc_new;
+    tcc[1]._delete = tcc_delete;
+    tcc[1]._set_error_func = tcc_set_error_func;
+    tcc[1]._set_options = tcc_set_options;
+    tcc[1]._set_output_type = tcc_set_output_type;
+    tcc[1]._define_symbol = tcc_define_symbol;
+    tcc[1]._compile_string = tcc_compile_string;
+    tcc[1]._add_symbol = tcc_add_symbol;
+    tcc[1]._add_library = tcc_add_library;
+    tcc[1]._relocate = tcc_relocate;
+    tcc[1]._get_symbol = tcc_get_symbol;
+
+    printf("%0X %0X\n", tcc[0]._new, tcc[1]._new);
+  }
+
+  bool compile(const string &code, string *err) {
 #if _USE_TRY_LOCK
     if (tryLock && !lock.try_lock()) {
       string("No Lock");
@@ -144,6 +227,9 @@ struct SwappingCompilerImplementation {
 
     bool compileSucceeded = false;
 
+#ifdef USE_DYNAMIC
+    printf("compiling with %s\n", ((1 - active) == 0) ? "dynamic" : "static");
+#endif
     if (tcc[1 - active].compile(code, err)) {
       this->size = tcc[1 - active].size;
       shouldSwap = true;
@@ -174,7 +260,7 @@ SwappingCompiler::~SwappingCompiler() { delete implementation; }
 
 int SwappingCompiler::size() { return implementation->size; }
 
-bool SwappingCompiler::operator()(const string& code, string* err) {
+bool SwappingCompiler::operator()(const string &code, string *err) {
   return implementation->compile(code, err);
 }
 
